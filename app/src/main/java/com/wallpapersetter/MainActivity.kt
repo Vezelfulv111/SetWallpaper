308326f7e0bdca521041defd5238c1d0c7ef62b9
Vezelfulv111 <anatolii-kozlov@list.ru>
2023-02-04 02:13:10 +0300

    [u] : Switched to Fragment logic

--- app/src/main/java/com/wallpapersetter/MainActivity.kt
+++ app/src/main/java/com/wallpapersetter/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.wallpapersetter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Insets.add
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            setContentView(R.layout.activity_main)
+                val fm: FragmentManager = supportFragmentManager
+                var fragment = fm.findFragmentByTag("MainScreenFragment")
+            if (fragment == null) {
+                val ft: FragmentTransaction = fm.beginTransaction()
+                fragment = MainScreenFragment()
+                ft.add(android.R.id.content, fragment as MainScreenFragment, "MainScreenFragment")
+                ft.commit()
+            }
+        }, 1000)
     }
 }
---
+++ app/src/main/java/com/wallpapersetter/MainScreenFragment.kt
@@ -0,0 +1,76 @@
+package com.wallpapersetter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+
+
+class MainScreenFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view: View = inflater.inflate(R.layout.fragment_main_screen, container, false)
+        val BUTTON_IDS = intArrayOf(
+            R.id.imageButton1,
+            R.id.imageButton2,
+            R.id.imageButton3,
+            R.id.imageButton4,
+            R.id.imageButton5,
+            R.id.imageButton6,
+        )
+        val buttonList: ArrayList<ImageButton> = ArrayList(6)
+        for (id in BUTTON_IDS) {
+            val button = view.findViewById<View>(id) as ImageButton
+            buttonList.add(button)
+            updateBin(id)
+        }
+        return view
+}
+
+    private fun updateBin(id : Int) {
+        val client = OkHttpClient()
+        val  key = "33106230-b104905cd7ff74ed17e2229af"
+        val category = "backgrounds"
+        val requestUrl ="https://pixabay.com/api/?key=$key&category=$category&safesearch=true&per_page=3"
+        var imageUrl = ""
+        val request: Request = Request.Builder()
+            .url(requestUrl)
+            .get()
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        throw IOException("Unexpected code $response")
+
+                    } else {
+                        val str = response.body()!!.string()
+                        val jsonData = JSONObject(str)//получили json массив
+                        val hits = jsonData.get("hits") as JSONArray
+                        val imageObject = hits[0] as JSONObject//одно изображение
+                        imageUrl = imageObject.get("webformatURL") as String//ссылка на изображение
+
+                        if (imageUrl.isNotEmpty()) {
+                            val imageButton = view?.findViewById<ImageButton>(id)
+                            activity?.runOnUiThread(Runnable {
+                                Picasso.get().load(imageUrl).into(imageButton);
+                            })
+                        }
+
+                    }
+                }
+            }
+        })
+    }
+
+}
